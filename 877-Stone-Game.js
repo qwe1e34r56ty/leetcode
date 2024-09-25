41c0cfe21c0700f6cabd8081473a24398b68571e
qwe1e34r56ty <134710541+qwe1e34r56ty@users.noreply.github.com>
2024-09-25 09:24:01 +0900

    [877] [Time Beats: 7.50%] [Memory Beats: 100.00%] - LeetPush

--- 877-Stone-Game.js
+++ 877-Stone-Game.js
@@ -3,5 +3,32 @@
  * @return {boolean}
  */
 var stoneGame = function(piles) {
-    return true;
+    var pile_len = piles.length;
+    var memo = new Array(pile_len);
+    var answer = undefined;
+    for(var i = 0; i < pile_len; i++){
+        memo[i] = new Array(pile_len).fill(-Infinity);
+    }
+    dp(0, pile_len - 1);
+    if(memo[0][pile_len - 1] > 0){
+        answer = true;
+    }
+    else{
+        answer = false;
+    }
+    return answer;
+    function dp(i, j){
+        var ret = -Infinity;
+        if(i > j){
+            return 0;
+        }
+        if(memo[i][j] != -Infinity){
+            ret = memo[i][j];
+            return ret;
+        }
+        ret = Math.max(ret, piles[i] - dp(i + 1, j));
+        ret = Math.max(ret, piles[j] - dp(i, j - 1));
+        memo[i][j] = ret;
+        return ret;
+    }
 };
