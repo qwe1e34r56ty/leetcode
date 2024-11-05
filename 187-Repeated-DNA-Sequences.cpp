3e93f5682d71b520cac18059d9c9dc1318438eda
qwe1e34r56ty <134710541+qwe1e34r56ty@users.noreply.github.com>
2024-11-05 18:46:36 +0900

    [187] [Time Beats: 24.94%] [Memory Beats: 22.63%] - LeetPush

--- 187-Repeated-DNA-Sequences.cpp
+++ 187-Repeated-DNA-Sequences.cpp
@@ -5,20 +5,6 @@ const int T = 3;
 
 class Solution {
 public:
-    string toString(int cur){
-        string ret{};
-        for(int i = 0; i < 10; i++){
-            switch(cur & 0b11){
-                case A: ret.push_back('A'); break;
-                case C: ret.push_back('C'); break;
-                case G: ret.push_back('G'); break;
-                case T: ret.push_back('T'); break;
-            }
-            cur >>= 2;
-        }
-        reverse(ret.begin(), ret.end());
-        return ret;
-    }
     vector<string> findRepeatedDnaSequences(string s) {
         vector<int> table(1<<20, 0);
         vector<int> bit(128, 0);
@@ -28,27 +14,22 @@ public:
         bit['G'] = 0b10;
         bit['T'] = 0b11;
         int cur = 0;
-        for(int i = 0; i < 10; i++){
-            cur <<= 2;
-            cur |= bit[s[i]];
-        }
+        for(int i = 0; i < 10; i++)
+            cur = (cur << 2) | bit[s[i]];
         table[cur] = 1;
+        int mask = (1 << 20) - 1;
         for(int i = 10; i < s.length(); i++){
-            cur <<= 2;
-            cur |= bit[s[i]];
-            cur &= (1 << 20) - 1;
+            cur = ((cur << 2) | bit[s[i]]) & mask;
             if(table[cur] == 1){
-                answer.push_back(toString(cur));
+                answer.push_back(s.substr(i - 9, 10));
                 table[cur] = 2;
             }
             else if(table[cur] == 2){
-                
+
             }
-            else{
+            else
                 table[cur] = 1;
-            }
-        }
-        
+        } 
         return answer;
     }
 };
