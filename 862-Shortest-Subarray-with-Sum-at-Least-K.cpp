6d3b35c46f91882e9084b30c8fc0e832dab18545
qwe1e34r56ty <134710541+qwe1e34r56ty@users.noreply.github.com>
2024-11-17 17:43:52 +0900

    [862] [Time Beats: 84.44%] [Memory Beats: 88.81%] - LeetPush

--- 862-Shortest-Subarray-with-Sum-at-Least-K.cpp
+++ 862-Shortest-Subarray-with-Sum-at-Least-K.cpp
@@ -10,16 +10,20 @@ public:
         deque<int> queue;
         int len = nums.size();
         int ans = len + 1;
+        vector<long long> nums_l(nums.size(), 0);
+        for(int i = 0; i < nums.size(); i++){
+            nums_l[i] = nums[i];
+        }
         for(int i = 0; i < len; i++){
-            nums[i] += (i > 0) ? nums[i - 1] : 0;
-            if(nums[i] >= k){
+            nums_l[i] += (i > 0) ? nums_l[i - 1] : 0;
+            if(nums_l[i] >= k){
                 ans = min(ans, i + 1);
             }
-            while(queue.size() && nums[i] - nums[queue.front()] >= k){
+            while(queue.size() && nums_l[i] - nums_l[queue.front()] >= k){
                 ans = min(ans, i - queue.front());
                 queue.pop_front();
             }
-            while(queue.size() && nums[queue.back()] >= nums[i]){
+            while(queue.size() && nums_l[queue.back()] >= nums_l[i]){
                 queue.pop_back();
             }
             queue.push_back(i);
