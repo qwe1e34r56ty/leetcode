fd974a7a145658439478add3f4b98cb2b400f93a
qwe1e34r56ty <134710541+qwe1e34r56ty@users.noreply.github.com>
2024-11-26 16:25:13 +0900

    [773] [Time Beats: 59.11%] [Memory Beats: 80.91%] - LeetPush

---
+++ 773-Sliding-Puzzle.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int slidingPuzzle(vector<vector<int>>& board) {
+        vector<vector<int>> dir = {{1, 3}, {0, 2, 4}, {1, 5}, {0, 4}, {1, 3, 5}, {2, 4}};
+        string target = "123450";
+        unordered_set<string> vis;
+        queue<string> q;
+        string start = "";
+
+        for(const auto& i : board){
+            for(int j : i){
+                start += to_string(j);
+            }
+        }
+
+        q.push(start);
+        vis.insert(start);
+        int step = 0;
+
+        while (!q.empty()) {
+            int size = q.size();
+            while (size--) {
+                string current = q.front();
+                q.pop();
+
+                if (current == target) return step;
+
+                int zero = current.find('0');
+
+                for (auto move : dir[zero]) {
+                    string next = current;
+                    swap(next[move], next[zero]);
+                    if (!vis.contains(next)) {
+                        vis.insert(next);
+                        q.push(next);
+                    }
+                }
+            }
+            step++;
+        }
+        cout << start << endl;
+        return -1;
+    }
+};
