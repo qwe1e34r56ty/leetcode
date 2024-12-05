89e42e11498e20adeecb5b03720ca1fd9b00ae88
qwe1e34r56ty <134710541+qwe1e34r56ty@users.noreply.github.com>
2024-12-05 19:26:18 +0900

    [2337] [Time Beats: 77.01%] [Memory Beats: 35.59%] - LeetPush

--- 2337-Move-Pieces-to-Obtain-a-String.cpp
+++ 2337-Move-Pieces-to-Obtain-a-String.cpp
@@ -4,17 +4,20 @@ public:
         int i = 0;
         int l = 0;
         int r = 0;
-        char s_top = '_';
-        char t_top = '_';
+        bool c_1;
+        bool c_2;
+        bool c_3;
+        bool c_4;
         for(int i = 0; i < start.length(); i++){
             l += start[i] == 'L';
             l -= target[i] == 'L';
             r += start[i] == 'R';
             r -= target[i] == 'R';
-            if(r < 0) return false;
-            if(l > 0) return false;
-            if(r > 0 && target[i] == 'L') return false;
-            if(l < 0 && start[i] == 'R') return false;
+            c_1 = r < 0;
+            c_2 = l > 0;
+            c_3 = r > 0 && target[i] == 'L';
+            c_4 = l < 0 && start[i] == 'R';
+            if(c_1 || c_2 || c_3 || c_4) return false;
         }
         return l == r;
     }
