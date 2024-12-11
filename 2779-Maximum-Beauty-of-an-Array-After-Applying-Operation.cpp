c1b1c80f5ca4d5491534b5759d31305556d7d0df
qwe1e34r56ty <134710541+qwe1e34r56ty@users.noreply.github.com>
2024-12-11 15:12:11 +0900

    [2779] [Time Beats: 5.14%] [Memory Beats: 48.58%] - LeetPush

---
+++ 2779-Maximum-Beauty-of-an-Array-After-Applying-Operation.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        queue<int> q;
+        int ans = 0;
+        for(int i = 0; i < nums.size(); i++){
+            while(!q.empty() && q.front() + 2 * k < nums[i]){
+                q.pop();
+            }
+            if(q.empty()){
+                ans = max(ans, 1);
+            }
+            else{
+                ans = max(ans, (int)q.size() + 1);
+                cout << ans << endl;
+            }
+            q.push(nums[i]);
+        }
+        return ans;
+    }
+};
